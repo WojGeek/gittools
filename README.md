c38552687d7c55dda26b079fc1bc41dfa3310a12
Willians Patiño <ppwj@yahoo.com>
2024-04-18 16:53:58 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,20 +1,53 @@
 ## Facilitar la creación de un repositorio en Github
 
-   Objetivo: Hacer el primer commit de manera simple
+> Objetivo: Simplificar lo cotidiano cuando actualizo un repositorio Github.
 
 
-#### En la consola
+#### Sugerencia de instalación
+
+> Clonar el repositorio desde la URL
 
 ```
-  $ ./do-first-commit.sh    repository
+    $  cd ~/
+    $  git clone https://github.com/WojGeek/gitools.git
+
+```
+
+#### Requerimientos antes de utilizar los scripts
+>
+> - Crear un TOKEN en su cuenta Github:  https://github.com/settings/tokens
+> 
+> - Declarar dos variables de entorno 
+> 
+>>   MY_TOKEN="jysh_7132kasasTOKEN"
+>>   GITHUB_ACCOUNT="your-username"
+>
+>   en el archivo **my_custom.env**  en  el directorio clonado: **gittools**
+>
+>
+
+#### 1. Hacer el primer commit de manera simple
+
+
+```
+     $   ~/gittools/do-first-commit.sh    repository
 
 ```
 
 
+#### 2.  Actualizar cambios en su repositorio
 
+>> **Opcional**
+> 
+> ya que usualmente podria como de costumbre
+> - git add.
+> - git commit -m "your comment"
+> - git push
+>
+>> Sin embargo, hay caso que se requiere el token para **git push**
+>
 
+```
+     $   ~/gittools/update-repo.sh   repository
 
-
-
-
-
+```
---
+++ update-repo.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+#  Facilitar la creación de un repositorio en Github
+#           18.4.2024, 15:50
+
+# Do an easy repository update
+
+clear
+
+WEBURL='github.com'
+
+# Token and account taken from .env
+source ~/gittools/my_custom.env
+
+# repository name
+REPO=$1
+
+
+
+# Check token
+if [ -z "$MY_TOKEN" ] ; then
+    
+    echo -e " \n Aviso: \n •  TOKEN no existe - Debe ser creado en  https://github.com/settings/tokens\n"
+    echo -e " \t- Obtenga el token y declare en el archivo 'my_custom.env' como ejemplo: \n"
+    echo -e "  \t MY_TOKEN=\"pkEr_3223423432423aadasd34\" \n"
+    exit 0
+else
+    #echo -e "My token: $MY_TOKEN"
+    echo -e " • Token: ${MY_TOKEN:0:5}...${MY_TOKEN: -7}"
+fi
+
+# check Github account
+if [ -z "$GITHUB_ACCOUNT" ] ; then
+    
+    echo -e "\n Aviso: \n •  Cuenta no existe - Requiere especificar el usuario Github\n "
+    echo -e " \t- Declare en el archivo 'my_custom.env' como: \n"
+    echo -e "  \t GITHUB_ACCOUNT=\"billgates\" \n"
+    exit 0
+else
+    echo -e " • Cuenta: $GITHUB_ACCOUNT"
+fi
+
+
+
+if [ -z "$REPO" ] ;  then
+    
+    
+    echo -e "\n\t$0    repository \n"
+    echo -e "Aviso: \n •  Espeficique el nombre del repositorio"
+    exit 0
+else
+    echo -e " • Repositorio: $REPO"
+fi
+
+
+echo -e "\n Updating repo..."
+
+
+# echo -e  " • git init"
+# #git init
+
+# echo  " • add ."
+# #git add .
+
+# echo -e " • git commit -m \"$COMMENT\" "
+# #git commit -m "$COMMENT"
+
+# echo  " • branch -M main "
+# #git branch -M main
+
+
+
+# echo -e " • git remote add origin https://$WEBURL/$GITHUB_ACCOUNT/$REPO.git"
+# ##git remote add origin https://$WEBURL/$GITHUB_ACCOUNT/$REPO.git
+
+
+echo -e " • git remote set-url origin https://$MY_TOKEN@$WEBURL/$GITHUB_ACCOUNT/$REPO.git "
+git remote set-url origin https://$MY_TOKEN@$WEBURL/$GITHUB_ACCOUNT/$REPO.git
+
+echo " • git push "
+git push
+
+#git remote set-url origin https://$MY_TOKEN@$WEBURL/$GITHUB_ACCOUNT/$REPO.git  > /dev/null 2>&1)
+
+# Si falla la ejecución del comando...
+# if [[ $do_ -eq 0 ]]; then
+#     echo -e "Falló el commit, revise los parámetros"
+#     exit 1
+# fi
+
